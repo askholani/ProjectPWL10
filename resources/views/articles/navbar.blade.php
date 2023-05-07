2bb21050fe8ce66809b1bbf5542ca3fc8cc9999a
askholani <askholani210@gmail.com>
2023-05-07 09:40:30 +0700

    praktikum 3

--- resources/views/articles/navbar.blade.php
+++ resources/views/articles/navbar.blade.php
@@ -9,7 +9,7 @@
              <a href="" class="nav-link">Home</a>
          </li>
          <li class="nav-item d-none d-sm-inline-block">
-             <a href="#" class="nav-link">Contact</a>
+             <a href="#" class="nav-link">Contacts</a>
          </li>
          <li class="nav-item d-none d-sm-inline-block">
              <a href="{{ route('articles.create') }}" class="nav-link">Tambah</a>
